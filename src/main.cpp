7aa9a8a5a5a642c114a4f7c8a4b2ee25c98b0a51
Nicolas Bayle <nicolas.bayle20@imperial.ac.uk>
2023-03-05 17:28:16 +0000

    const and grid

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+double spacialIntegration() {
+    return 0.0;
+}
+
+int main(int argc, char* argv[]) {
+    const int nx = 100; const int ny = 100;
+    const double dt = 0.1;
+    const double t = 10.0;
+    const int ic = 1;
+
+    double* grid = new double[nx * ny];
+
+    delete[] grid;
+    return 0;
+}
