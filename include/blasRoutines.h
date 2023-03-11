ee5c322aafeaeca834bc693dbe755a8bde67e560
Nicolas Bayle <nicolas.bayle20@imperial.ac.uk>
2023-03-11 16:00:22 +0000

    added dscal

--- include/blasRoutines.h
+++ include/blasRoutines.h
@@ -1,4 +1,16 @@
 #ifndef BLAS_ROUTINES_H
 #define BLAS_ROUTINES_H
 
+#define F77NAME(x) x##_
+
+extern "C" {
+    // x = alpha * x
+    void F77NAME(dscal) (
+        const int& n,
+        const double& alpha,
+        double* x,
+        const int& incx
+    );
+}
+
 #endif // BLAS_ROUTINES_H
