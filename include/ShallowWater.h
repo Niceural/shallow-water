9540194e37db9096ce0390578b3093f0b0c5acc0
Nicolas Bayle <nicolas.bayle20@imperial.ac.uk>
2023-03-12 14:11:23 +0000

    central difference

--- include/ShallowWater.h
+++ include/ShallowWater.h
@@ -3,11 +3,13 @@
 
 #include <cmath>
 #include "blasRoutines.h"
+#include <stdexcept>
 
 class ShallowWater {
     private:
+        // inputs
         double _dt; /// Time-step to use.
-        int _t; /// Total integration time.
+        double _t; /// Total integration time.
         int _nx; /// Number of grid points in x.
         int _ny; /// Number of grid points in y.
         int _ic; /// Index of the initial condition to use (1-4).
@@ -17,12 +19,19 @@ class ShallowWater {
         double _dy; /// Constant point spacing in y.
         double _g; /// Acceleration due to gravity.
 
+        // 
         double* _u; /// Matrix of size (nx * ny) of x-component of velocity at each grid point.
         double* _v; /// Matrix of size (nx * ny) of y-component of velocity at each grid point.
         double* _h; /// Matrix of size (nx * ny) of surface height, relative to a mean height of zero, at each grid point.
 
+        // central difference
+        int _ncd;
+        int _mcd;
+        int _ldcd;
+        double* _cd; /// Banded matrix to perform the 6th-order central difference scheme with respect to x (size: nx-2 x ny).
+
     public:
-        ShallowWater(double dt, int t, int nx, int ny, int nc);
+        ShallowWater(double dt, double t, int nx, int ny, int nc);
         ~ShallowWater();
 
         void setInitialConditions();
--- src/ShallowWater.cpp
+++ src/ShallowWater.cpp
@@ -1,7 +1,7 @@
 #include "../include/ShallowWater.h"
 
 ShallowWater::ShallowWater(
-    double dt, int t,
+    double dt, double t,
     int nx, int ny, 
     int ic
 ):
@@ -9,15 +9,34 @@ ShallowWater::ShallowWater(
     _nx(nx), _ny(ny),
     _ic(ic),
     _n(nx*ny), _dx(1.0), _dy(1.0), _g(9.81),
-    _u(new double[_n]),
-    _v(new double[_n]),
-    _h(new double[_n])
-{}
+    _u(new double[_n]), _v(new double[_n]), _h(new double[_n]),
+    _ncd(_nx-2), _mcd(_nx), _ldcd(7), _cd(new double[_ncd*_mcd])
+{
+    // check inputs
+    if (
+        _dt < 0.0 || _t < 0.0 || _t < _dt ||
+        _nx < 2 || _ny < 2 || _nx != _ny ||
+        _ic < 1 || 4 < _ic
+    ) throw std::invalid_argument("Invalid argument.");
+
+    // central difference matrix
+    double val[] = {
+        1.0/60.0/_dx, -3.0/20.0/_dx, 3.0/4.0/_dx, 
+        0.0,
+        -3.0/4.0/_dx, 3.0/20.0/_dx, -1.0/60.0/_dx
+    };
+    for (int i = 0; i < _ncd; i++) {
+        for (int j = 0; j < _ldcd; j++) {
+            _cd[i*_ldcd + j] = val[j];
+        }
+    }
+}
 
 ShallowWater::~ShallowWater() {
     delete[] _u;
     delete[] _v;
     delete[] _h;
+    delete[] _cd;
 }
 
 void ShallowWater::setInitialConditions() {
