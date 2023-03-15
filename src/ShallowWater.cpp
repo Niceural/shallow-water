66f8f563fdbe2a5e505965d47722be407f02385b
Nicolas Bayle <nicolas.bayle20@imperial.ac.uk>
2023-03-15 16:13:32 +0000

    runge kutta coded

--- src/ShallowWater.cpp
+++ src/ShallowWater.cpp
@@ -190,24 +190,158 @@ void ShallowWater::setInitialConditions() {
 void ShallowWater::timeIntegrate() {
     setInitialConditions();
 
-    GeneralMatrix k1 = GeneralMatrix(_nx, _ny);
-    GeneralMatrix k2 = GeneralMatrix(_nx, _ny);
-    GeneralMatrix k3 = GeneralMatrix(_nx, _ny);
-    GeneralMatrix k4 = GeneralMatrix(_nx, _ny);
+    // central difference
+    GeneralMatrix dUdx(_nx, _ny);
+    GeneralMatrix dUdy(_nx, _ny);
+    GeneralMatrix dVdx(_nx, _ny);
+    GeneralMatrix dVdy(_nx, _ny);
+    GeneralMatrix dHdx(_nx, _ny);
+    GeneralMatrix dHdy(_nx, _ny);
+
+    // runge kutta U
+    GeneralMatrix k1U(_nx, _ny);
+    GeneralMatrix k2U(_nx, _ny);
+    GeneralMatrix k3U(_nx, _ny);
+    GeneralMatrix k4U(_nx, _ny);
+
+    // runge kutta V
+    GeneralMatrix k1V(_nx, _ny);
+    GeneralMatrix k2V(_nx, _ny);
+    GeneralMatrix k3V(_nx, _ny);
+    GeneralMatrix k4V(_nx, _ny);
+
+    // runge kutta H
+    GeneralMatrix k1H(_nx, _ny);
+    GeneralMatrix k2H(_nx, _ny);
+    GeneralMatrix k3H(_nx, _ny);
+    GeneralMatrix k4H(_nx, _ny);
+
+    // temporary variables
+    GeneralMatrix tempU(_nx, _ny);
+    GeneralMatrix tempV(_nx, _ny);
+    GeneralMatrix tempH(_nx, _ny);
 
     double t = 0.0;
     while (t < _t) {
         // perform central difference
-        integrateWrtX(_U, _dUdx);
-        integrateWrtY(_U, _dUdy);
 
-        // k1 = f(u_n, v_n, k_n)
-        _U.elementwiseMultiplication(-1.0, _dUdx, 0.0, k1); // k1 = - u * dUdx
-        _V.elementwiseMultiplication(-1.0, _dUdy, 1.0, k1); // k1 = k1 - v * dUdy
-        F77NAME(daxpy)(k1.size(), -_g, _dHdx.getPointer(0), 1, k1.getPointer(0), 1); // k1 = k1 - g*dHdx
+        //--------- k1
+        // copy U, V, H, to tempU, tempV, tempH
+        F77NAME(dcopy)(_U.size(), _U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(dcopy)(_V.size(), _V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(dcopy)(_H.size(), _H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // k1U
+        F77NAME(dcopy)(dHdx.size(), dHdx.getPointer(0), 1, k1U.getPointer(0), 1); // k1U = dHdx
+        F77NAME(dscal)(k1U.size(), -_g, k1U.getPointer(0), 1); // k1U *= -g
+        elementWiseMultiplicationLoop(-1.0, tempU, dUdx, 1.0, k1U); // k1U -= U * dUdx
+        elementWiseMultiplicationLoop(-1.0, tempV, dUdy, 1.0, k1U); // k1U -= V * dUdy
+        // k1V
+        F77NAME(dcopy)(dHdy.size(), dHdy.getPointer(0), 1, k1V.getPointer(0), 1); // k1V = dHdy
+        F77NAME(dscal)(k1V.size(), -_g, k1V.getPointer(0), 1); // k1V *= -g
+        elementWiseMultiplicationLoop(-1.0, tempU, dVdx, 1.0, k1V); // k1V -= U * dVdx
+        elementWiseMultiplicationLoop(-1.0, tempV, dVdy, 1.0, k1V); // k1V -= V * dVdy
+        // k1H
+        elementWiseMultiplicationLoop(-1.0, tempU, dHdx, 0.0, k1H);
+        elementWiseMultiplicationLoop(-1.0, tempH, dUdx, 1.0, k1H);
+        elementWiseMultiplicationLoop(-1.0, tempV, dHdy, 1.0, k1H);
+        elementWiseMultiplicationLoop(-1.0, tempH, dVdy, 1.0, k1H);
 
-        // k2 = f(u_n + dt*k1/2, v_n, h_n)
-        
+        //--------- k2
+        // copy U, V, H, to tempU, tempV, tempH
+        F77NAME(dcopy)(_U.size(), _U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(dcopy)(_V.size(), _V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(dcopy)(_H.size(), _H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // a_n += dt * k1 / 2
+        F77NAME(daxpy)(tempU.size(), _dt*0.5, k1U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(daxpy)(tempV.size(), _dt*0.5, k1V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(daxpy)(tempH.size(), _dt*0.5, k1H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // k2U
+        F77NAME(dcopy)(dHdx.size(), dHdx.getPointer(0), 1, k2U.getPointer(0), 1); // k2U = dHdx
+        F77NAME(dscal)(k2U.size(), -_g, k2U.getPointer(0), 1); // k2U *= -g
+        elementWiseMultiplicationLoop(-1.0, tempU, dUdx, 1.0, k2U); // k2U -= U * dUdx
+        elementWiseMultiplicationLoop(-1.0, tempV, dUdy, 1.0, k2U); // k2U -= V * dUdy
+        // k2V
+        F77NAME(dcopy)(dHdy.size(), dHdy.getPointer(0), 1, k2V.getPointer(0), 1); // k2V = dHdy
+        F77NAME(dscal)(k2V.size(), -_g, k2V.getPointer(0), 1); // k2V *= -g
+        elementWiseMultiplicationLoop(-1.0, tempU, dVdx, 1.0, k2V); // k2V -= U * dVdx
+        elementWiseMultiplicationLoop(-1.0, tempV, dVdy, 1.0, k2V); // k2V -= V * dVdy
+        // k2H
+        elementWiseMultiplicationLoop(-1.0, tempU, dHdx, 0.0, k2H);
+        elementWiseMultiplicationLoop(-1.0, tempH, dUdx, 1.0, k2H);
+        elementWiseMultiplicationLoop(-1.0, tempV, dHdy, 1.0, k2H);
+        elementWiseMultiplicationLoop(-1.0, tempH, dVdy, 1.0, k2H);
+
+        //--------- k3
+        // copy U, V, H, to tempU, tempV, tempH
+        F77NAME(dcopy)(_U.size(), _U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(dcopy)(_V.size(), _V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(dcopy)(_H.size(), _H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // a_n += dt * k2 / 2
+        F77NAME(daxpy)(tempU.size(), _dt*0.5, k2U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(daxpy)(tempV.size(), _dt*0.5, k2V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(daxpy)(tempH.size(), _dt*0.5, k2H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // k3U
+        F77NAME(dcopy)(dHdx.size(), dHdx.getPointer(0), 1, k3U.getPointer(0), 1); // k3U = dHdx
+        F77NAME(dscal)(k3U.size(), -_g, k3U.getPointer(0), 1); // k3U *= -g
+        elementWiseMultiplicationLoop(-1.0, tempU, dUdx, 1.0, k3U); // k3U -= U * dUdx
+        elementWiseMultiplicationLoop(-1.0, tempV, dUdy, 1.0, k3U); // k3U -= V * dUdy
+        // k3V
+        F77NAME(dcopy)(dHdy.size(), dHdy.getPointer(0), 1, k3V.getPointer(0), 1); // k3V = dHdy
+        F77NAME(dscal)(k3V.size(), -_g, k3V.getPointer(0), 1); // k3V *= -g
+        elementWiseMultiplicationLoop(-1.0, tempU, dVdx, 1.0, k3V); // k3V -= U * dVdx
+        elementWiseMultiplicationLoop(-1.0, tempV, dVdy, 1.0, k3V); // k3V -= V * dVdy
+        // k3H
+        elementWiseMultiplicationLoop(-1.0, tempU, dHdx, 0.0, k3H);
+        elementWiseMultiplicationLoop(-1.0, tempH, dUdx, 1.0, k3H);
+        elementWiseMultiplicationLoop(-1.0, tempV, dHdy, 1.0, k3H);
+        elementWiseMultiplicationLoop(-1.0, tempH, dVdy, 1.0, k3H);
+
+        //--------- k4
+        // copy U, V, H, to tempU, tempV, tempH
+        F77NAME(dcopy)(_U.size(), _U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(dcopy)(_V.size(), _V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(dcopy)(_H.size(), _H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // a_n += dt * k3 / 2
+        F77NAME(daxpy)(tempU.size(), _dt, k3U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(daxpy)(tempV.size(), _dt, k3V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(daxpy)(tempH.size(), _dt, k3H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // k4U
+        F77NAME(dcopy)(dHdx.size(), dHdx.getPointer(0), 1, k4U.getPointer(0), 1); // k4U = dHdx
+        F77NAME(dscal)(k4U.size(), -_g, k4U.getPointer(0), 1); // k4U *= -g
+        elementWiseMultiplicationLoop(-1.0, tempU, dUdx, 1.0, k4U); // k4U -= U * dUdx
+        elementWiseMultiplicationLoop(-1.0, tempV, dUdy, 1.0, k4U); // k4U -= V * dUdy
+        // k4V
+        F77NAME(dcopy)(dHdy.size(), dHdy.getPointer(0), 1, k4V.getPointer(0), 1); // k4V = dHdy
+        F77NAME(dscal)(k4V.size(), -_g, k4V.getPointer(0), 1); // k4V *= -g
+        elementWiseMultiplicationLoop(-1.0, tempU, dVdx, 1.0, k4V); // k4V -= U * dVdx
+        elementWiseMultiplicationLoop(-1.0, tempV, dVdy, 1.0, k4V); // k4V -= V * dVdy
+        // k4H
+        elementWiseMultiplicationLoop(-1.0, tempU, dHdx, 0.0, k4H);
+        elementWiseMultiplicationLoop(-1.0, tempH, dUdx, 1.0, k4H);
+        elementWiseMultiplicationLoop(-1.0, tempV, dHdy, 1.0, k4H);
+        elementWiseMultiplicationLoop(-1.0, tempH, dVdy, 1.0, k4H);
+
+        // add k1
+        F77NAME(dcopy)(k1U.size(), k1U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(dcopy)(k1V.size(), k1V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(dcopy)(k1H.size(), k1H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // add k2
+        F77NAME(daxpy)(tempU.size(), 2.0, k2U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(daxpy)(tempV.size(), 2.0, k2V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(daxpy)(tempH.size(), 2.0, k2H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // add k3
+        F77NAME(daxpy)(tempU.size(), 2.0, k3U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(daxpy)(tempV.size(), 2.0, k3V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(daxpy)(tempH.size(), 2.0, k3H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // add k4
+        F77NAME(daxpy)(tempU.size(), 1.0, k4U.getPointer(0), 1, tempU.getPointer(0), 1);
+        F77NAME(daxpy)(tempV.size(), 1.0, k4V.getPointer(0), 1, tempV.getPointer(0), 1);
+        F77NAME(daxpy)(tempH.size(), 1.0, k4H.getPointer(0), 1, tempH.getPointer(0), 1);
+        // add to U, V, and H
+        F77NAME(daxpy)(_U.size(), 1.0/6.0*_dt, tempU.getPointer(0), 1, _U.getPointer(0), 1);
+        F77NAME(daxpy)(_V.size(), 1.0/6.0*_dt, tempV.getPointer(0), 1, _V.getPointer(0), 1);
+        F77NAME(daxpy)(_H.size(), 1.0/6.0*_dt, tempH.getPointer(0), 1, _H.getPointer(0), 1);
+
         t += _dt;
     }
 }
